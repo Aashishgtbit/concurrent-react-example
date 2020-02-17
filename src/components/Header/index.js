72d4da12ab0a66f058e394ad98fca4c445a1c36a
Aashishgtbit <aashishtiwari97@gmail.com>
2020-02-17 13:08:03 +0530

    removed radio buttons from header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 import "./style.scss";
 export default function Header() {
-  const [modeValue, setModeValue] = useState(
-    window.location.pathname.includes("/normal-mode-example")
-      ? "Normal Mode"
-      : "Concurrent Mode"
-  );
-  const handleModeChange = event => {
-    console.log("mode value", event.target.value);
-    if (event.target.value === "concurrentMode") {
-      setModeValue("Concurrent Mode");
-      window.location.replace("/concurrent-mode-example");
-    } else {
-      setModeValue("Normal Mode");
-      window.location.replace("/normal-mode-example");
-    }
-  };
+  // const [modeValue, setModeValue] = useState(
+  //   window.location.pathname.includes("/normal-mode-example")
+  //     ? "Normal Mode"
+  //     : "Concurrent Mode"
+  // );
+  // const handleModeChange = event => {
+  //   console.log("mode value", event.target.value);
+  //   if (event.target.value === "concurrentMode") {
+  //     setModeValue("Concurrent Mode");
+  //     window.location.replace("/concurrent-mode-example");
+  //   } else {
+  //     setModeValue("Normal Mode");
+  //     window.location.replace("/normal-mode-example");
+  //   }
+  // };
   return (
     <div className="wrapper-header">
-      <div>
+      {/* <div>
         <input
           type="radio"
           name="mode"
@@ -35,8 +35,8 @@ export default function Header() {
           onChange={handleModeChange}
         />{" "}
         Concurrent Mode
-      </div>
-      <h2> {modeValue} Example</h2>
+      </div> */}
+      <h2> Concurrent Mode Example</h2>
     </div>
   );
 }
